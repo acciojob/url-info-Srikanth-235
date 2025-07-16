2c096341d1a9e35495435f7cd8e376ddaf4cb241
Srikanth-235 <161904315+Srikanth-235@users.noreply.github.com>
2025-07-16 14:33:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //your JS code here. If required.
 const url1=location.href;
 const length1=url1.length;
-alert("The length of the URL is:"+length1);
+console.log(length1);
+//alert("The length of the URL is: "+length1);
